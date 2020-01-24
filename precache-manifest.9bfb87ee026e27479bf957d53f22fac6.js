425669f20152c8f011f9e25b9e6931c17f66b639
Guilherme Branco <guilhermejbranco@Guilhermes-MBP.lan>
2020-01-24 02:05:48 +0000

    Updates

---
+++ precache-manifest.9bfb87ee026e27479bf957d53f22fac6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b4d6df58c90ade359a44122fcb170b5",
+    "url": "/weather-advertio/index.html"
+  },
+  {
+    "revision": "e0f72e3d8656be63b426",
+    "url": "/weather-advertio/static/css/main.5ee6c206.chunk.css"
+  },
+  {
+    "revision": "c43770df56218f3b486c",
+    "url": "/weather-advertio/static/js/2.ebe49fa7.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/weather-advertio/static/js/2.ebe49fa7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e0f72e3d8656be63b426",
+    "url": "/weather-advertio/static/js/main.027c0396.chunk.js"
+  },
+  {
+    "revision": "6dedce52fa1ec4158de5",
+    "url": "/weather-advertio/static/js/runtime-main.7beae0df.js"
+  },
+  {
+    "revision": "9a636878c9d590bee5508764a8bea1bc",
+    "url": "/weather-advertio/static/media/logo.9a636878.svg"
+  }
+]);
